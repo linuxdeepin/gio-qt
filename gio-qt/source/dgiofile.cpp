2e0e0814488eff22c1ab5be17451c15ef0a84be4
Felix Yan <felixonmars@archlinux.org>
2023-09-15 18:23:40 +0300

    fix: use QElapsedTimer instead of QTime
    Fixes the following warning:
    ```
    /build/gio-qt/src/gio-qt-0.0.12/gio-qt/source/dgiofile.cpp:228:20: warning: ‘void QTime::start()’ is deprecated: Use QElapsedTimer instead [-Wdeprecated-declarations]
      228 |             t.start();
          |             ~~~~~~~^~
    ```

--- gio-qt/source/dgiofile.cpp
+++ gio-qt/source/dgiofile.cpp
@@ -16,7 +16,7 @@
 #include <QDebug>
 #include <QWaitCondition>
 #include <QMutex>
-#include <QTime>
+#include <QElapsedTimer>
 #include <QtConcurrent/QtConcurrentRun>
 #include <dgiomountoperation.h>
 
@@ -224,7 +224,7 @@ QExplicitlySharedDataPointer<DGioFileInfo> DGioFile::createFileInfo(QString attr
         QSharedPointer<QMutex> m(new QMutex);
         QtConcurrent::run([&, cond, m, timeout_msec] {
             Glib::RefPtr<FileInfo> localret;
-            QTime t;
+            QElapsedTimer t;
             t.start();
             try {
                 localret = d->getGmmFileInstance()->query_info(attr.toStdString(), flags);
